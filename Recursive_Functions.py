ef50dca105d7eddbf683cdf2f0014a8d283138d9
Nhinhoshxhy Desprez <noshdesprez@gmail.com>
2020-02-04 16:08:32 +0000

    same as previous method but with Dict.

--- Recursive_Functions.py
+++ Recursive_Functions.py
@@ -1,12 +1,30 @@
-look = [4, 5, 6, 7, 8, 'kjlsdhgfissacklgjsheu', 'ld', 'sd;']
+## WITH A LIST
+
+# look = [4, 5, 6, 7, 8, 'kjlsdhgfissacklgjsheu', 'ld', 'sd;']
 
 
-def recursive(a):
-  for x in a:
-      if isinstance(x, str):
-          if 'issac' in x:
-              return x
+# def recursive(a):
+#   for x in a:
+#       if isinstance(x, str):
+#           if 'issac' in x:
+#               return x
 
-find = recursive(look)
+# find = recursive(look)
 
+# print(find)
+
+## WITH A DICTIONARY
+
+g = {'sdf':'kkisaacd','dlk':432,'324':'dslkisaacl','dflkj':2343}
+
+def find(a):
+    lst = []
+    for value in a.values():
+        if isinstance(value, str):
+            if 'isaac' in value:
+                lst.append(value)
+ 
+    return lst
+
+find = find(g)
 print(find)
