a51f7d41e194b43060c4fb90d4b06ef97a9ca9eb
Nhinhoshxhy Desprez <noshdesprez@gmail.com>
2020-02-04 16:45:37 +0000

    using both methods of previous

--- Recursive_Functions.py
+++ Recursive_Functions.py
@@ -1,19 +1,19 @@
-## WITH A LIST
+# WITH A LIST
 
-# look = [4, 5, 6, 7, 8, 'kjlsdhgfissacklgjsheu', 'ld', 'sd;']
+look = [4, 5, 6, 7, 8, 'kjlsdhgfissacklgjsheu', 'ld', 'sd;']
 
 
-# def recursive(a):
-#   for x in a:
-#       if isinstance(x, str):
-#           if 'issac' in x:
-#               return x
+def recursive(a):
+  for x in a:
+      if isinstance(x, str):
+          if 'issac' in x:
+              return x
 
 # find = recursive(look)
 
 # print(find)
 
-## WITH A DICTIONARY
+# WITH A DICTIONARY
 
 g = {'sdf':'kkisaacd','dlk':432,'324':'dslkisaacl','dflkj':2343}
 
@@ -26,5 +26,30 @@ def find(a):
  
     return lst
 
-find = find(g)
-print(find)
+# find = find(g)
+# print(find)
+
+# IDENTIFIYING WETHER THE PARAM IS A LIST OR A DICT.
+
+n = [4, 5, 6, 7, 8, 'kjlsdhgfissacklgjsheu', 'ld', 'sd;']
+d = {'sdf':'kkisaacd','dlk':432,'324':'dslkisaacl','dflkj':2343}
+
+
+def find_lst(a):
+    if isinstance(a, list):
+        return recursive(a)
+    else:
+        return 'That is not a list'
+ 
+
+def find_dict(a):
+    if isinstance(a, dict):
+        return find(a)
+    else:
+        return 'That is not a dictionary'
+
+
+
+
+print(find_lst(n))
+print(find_dict(d))
